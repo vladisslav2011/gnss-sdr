915cdd358ec81c7a7b28871712bd8a33d1841696
Vlad P <vladisslav2011@gmail.com>
2022-07-17 13:06:32 +0300

    Try to prevent BeiDou ephemeris corruption

--- src/algorithms/telemetry_decoder/gnuradio_blocks/beidou_b1i_telemetry_decoder_gs.cc
+++ src/algorithms/telemetry_decoder/gnuradio_blocks/beidou_b1i_telemetry_decoder_gs.cc
@@ -45,6 +45,7 @@ struct Prev_Ephemeris
     std::shared_ptr<Beidou_Dnav_Ephemeris> valid_eph;
     std::shared_ptr<Beidou_Dnav_Ephemeris> last_eph;
     int valid_eph_count;
+    int valid_eph_thr;
 };
 
 
@@ -335,7 +336,8 @@ void beidou_b1i_telemetry_decoder_gs::decode_subframe(float *frame_symbols)
                                         {
                                             prev[tmp_obj->PRN].valid_eph = tmp_obj;
                                             prev[tmp_obj->PRN].valid_eph_count = 2;
-                                            pub = prev[tmp_obj->PRN].valid_eph_count >= EPH_PUB_THR;
+                                            pub = prev[tmp_obj->PRN].valid_eph_count >= prev[tmp_obj->PRN].valid_eph_thr;
+                                            prev[tmp_obj->PRN].valid_eph_thr = (EPH_PUB_THR > 2) ? EPH_PUB_THR : 2;
                                         }
                                 }
                             else
@@ -343,7 +345,8 @@ void beidou_b1i_telemetry_decoder_gs::decode_subframe(float *frame_symbols)
                                     if (dev_val < d_dev_thr)
                                         {
                                             prev[tmp_obj->PRN].valid_eph_count ++;
-                                            pub = prev[tmp_obj->PRN].valid_eph_count >= EPH_PUB_THR;
+                                            prev[tmp_obj->PRN].valid_eph_thr = (EPH_PUB_THR > 2) ? EPH_PUB_THR : 2;
+                                            pub = prev[tmp_obj->PRN].valid_eph_count >= prev[tmp_obj->PRN].valid_eph_thr;
                                         }
                                 }
                         }
@@ -351,7 +354,8 @@ void beidou_b1i_telemetry_decoder_gs::decode_subframe(float *frame_symbols)
                         {
                             prev[tmp_obj->PRN].valid_eph = tmp_obj;
                             prev[tmp_obj->PRN].valid_eph_count = 1;
-                            pub = prev[tmp_obj->PRN].valid_eph_count >= EPH_PUB_THR;
+                            prev[tmp_obj->PRN].valid_eph_thr = EPH_PUB_THR;
+                            pub = prev[tmp_obj->PRN].valid_eph_count >= prev[tmp_obj->PRN].valid_eph_thr;
                         }
                     prev[tmp_obj->PRN].last_eph = tmp_obj;
                     std::cout << "PRN "<<tmp_obj->PRN<<" dev_last = "<<dev_last<<" dev_val = "<<dev_val<<" count = "<<prev[tmp_obj->PRN].valid_eph_count<<"\n";
