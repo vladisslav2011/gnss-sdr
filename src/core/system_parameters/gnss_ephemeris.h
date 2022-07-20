9910d6972efefb924c271f10c313a208d196904e
Vlad P <vladisslav2011@gmail.com>
2022-07-20 05:10:41 +0300

    Generic  ephemeris validator

--- src/core/system_parameters/gnss_ephemeris.h
+++ src/core/system_parameters/gnss_ephemeris.h
@@ -21,11 +21,35 @@
 
 #include <array>
 #include <cstdint>
+#include <memory>
+#include <vector>
+
+class Common_Ephemeris
+{
+private:
+    using last_valid = struct
+    {
+        std::shared_ptr<Common_Ephemeris> last_eph;
+        std::shared_ptr<Common_Ephemeris> valid_eph;
+        int valid_eph_count;
+        int valid_eph_thr;
+    };
+
+protected:
+    static constexpr double DEVIATION_THRESHOLD = 0.00001;
+
+public:
+    using history_set = std::vector<last_valid>;
+    Common_Ephemeris() = default;
+    virtual double max_deviation(Common_Ephemeris &from) = 0;  //!< Compare a set of ephemeris to another one
+    static bool validate(history_set &hist, std::shared_ptr<Common_Ephemeris> eph, const int thr);
+    uint32_t PRN{};  //!< SV ID
+};
 
 /*!
  * \brief Base class for GNSS ephemeris storage
  */
-class Gnss_Ephemeris
+class Gnss_Ephemeris : public Common_Ephemeris
 {
 public:
     Gnss_Ephemeris() = default;
@@ -65,10 +89,9 @@ public:
      */
     double predicted_doppler(double rx_time_s, double lat, double lon, double h, double ve, double vn, double vu, int band) const;
 
-    void satellitePosition(double transmitTime);  //!< Computes the ECEF SV coordinates and ECEF velocity
-    double max_deviation(Gnss_Ephemeris& from);   //!< Compare a set of ephemeris to another one
+    void satellitePosition(double transmitTime);            //!< Computes the ECEF SV coordinates and ECEF velocity
+    double max_deviation(Common_Ephemeris &from) override;  //!< Compare a set of ephemeris to another one
 
-    uint32_t PRN{};     //!< SV ID
     double M_0{};       //!< Mean anomaly at reference time [rad]
     double delta_n{};   //!< Mean motion difference from computed value [rad/sec]
     double ecc{};       //!< Eccentricity
@@ -113,7 +136,7 @@ protected:
     char System{};  //!< Character ID of the GNSS system. 'G': GPS.  'E': Galileo.  'B': BeiDou
 
 private:
-    void satellitePosVelComputation(double transmitTime, std::array<double, 7>& pos_vel_dtr) const;
+    void satellitePosVelComputation(double transmitTime, std::array<double, 7> &pos_vel_dtr) const;
     double check_t(double time) const;
     double sv_clock_relativistic_term(double transmitTime) const;
 };
