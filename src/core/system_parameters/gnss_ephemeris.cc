3a506ee728dd6b18996370e8560cb838236f8b5d
Vladslav P <vladisslav2011@gmail.com>
2026-06-26 19:11:00 +0300

    Implement ephemeris-based acquisition assistance
    Signed-off-by: Vladislav P <vladisslav2011@gmail.com>

--- src/core/system_parameters/gnss_ephemeris.cc
+++ src/core/system_parameters/gnss_ephemeris.cc
@@ -140,6 +140,10 @@ double Gnss_Ephemeris::predicted_doppler(double rx_time_s,
                 {
                     predicted_doppler = predicted_doppler_normalized * FREQ3_BDS;
                 }
+            else if (band == 4)
+                {
+                    predicted_doppler = predicted_doppler_normalized * FREQ1;
+                }
             else
                 {
                     predicted_doppler = 0.0;
