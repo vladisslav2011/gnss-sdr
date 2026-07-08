a7501916b7ab78502b6fe2b65fedd2f38f42d0cb
Vladslav P <vladisslav2011@gmail.com>
2026-07-08 22:02:25 +0300

    gnss_almanac: BeiDou inclination correction
    Signed-off-by: Vladslav P <vladisslav2011@gmail.com>

--- src/core/system_parameters/gnss_almanac.cc
+++ src/core/system_parameters/gnss_almanac.cc
@@ -226,6 +226,7 @@ void Gnss_Almanac::satellitePosVelComputation(double transmitTime, std::array<do
     const double rkdot = a * this->ecc * sek * ekdot;
 
     // Correct inclination
+    bool skip_izero = (this->System == 'J') || ((this->System == 'C') && ((PRN > 0 && PRN < 6) || (PRN > 58)));
     double i;
     if (this->System == 'E')
         {
@@ -233,7 +234,7 @@ void Gnss_Almanac::satellitePosVelComputation(double transmitTime, std::array<do
         }
     else
         {
-            i = ((this->System == 'J') ? this->delta_i : (0.3 + this->delta_i)) * GNSS_PI;
+            i = (skip_izero ? this->delta_i : (0.3 + this->delta_i)) * GNSS_PI;
         }
 
     const double sik = sin(i);
